532bd2bca8909dceea693af889396c37298edeab
Gokarna Bhandari <gokarna.jung.bhandari@gmail.com>
2024-08-13 15:57:09 +0000

    insertPosition Done

--- src/main/java/InsertPosition.java
+++ src/main/java/InsertPosition.java
@@ -15,6 +15,12 @@ public class InsertPosition {
      * @return return the index that a number should be inserted into a sorted array.
      */
     public int searchInsert(int[] nums, int target) {
-        return -1;
+        if(nums[0] > target){
+            return 0;
+        }
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] < target && nums[i + 1] > target) return i + 1;
+        }
+        return nums.length;
     }
 }
